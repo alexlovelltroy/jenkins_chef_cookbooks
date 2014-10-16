42eb4cbb87a29329a6b1fe775886886f649baf8d
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-16 10:05:21 -0700

    set jenkins user and install plugins

--- site-cookbooks/bb_jenkins/attributes/default.rb
+++ site-cookbooks/bb_jenkins/attributes/default.rb
@@ -1,1 +1,15 @@
 normal['java']['jdk_version'] = '7'
+
+default[:jenkins][:password] = 'changeme'
+
+jenkins_plugin 'git'
+jenkins_plugin 'thinBackup'
+jenkins_plugin 'artifactdeployer'
+jenkins_plugin 'github'
+jenkins_plugin 'github-api'
+jenkins_plugin 'github-oauth'
+jenkins_plugin 'ghprb'
+jenkins_plugin 'mailer'
+jenkins_plugin 'parameterized-trigger'
+jenkins_plugin 's3'
+jenkins_plugin 'ws-cleanup'
--- site-cookbooks/bb_jenkins/recipes/master.rb
+++ site-cookbooks/bb_jenkins/recipes/master.rb
@@ -1,2 +1,7 @@
 include_recipe 'java'
 include_recipe 'jenkins::master'
+
+jenkins_password_credentials 'admin' do
+  description 'Admin'
+  password node[:jenkins][:password]
+end
