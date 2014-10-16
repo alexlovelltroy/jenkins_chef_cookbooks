371ff909d21e336cc0970d9df820dea492ac5b07
Taylor Ludwig <taylorludwig@gmail.com>
2014-10-16 10:12:50 -0700

    whoops, move plugin installs to recipe not attributes

--- site-cookbooks/bb_jenkins/recipes/master.rb
+++ site-cookbooks/bb_jenkins/recipes/master.rb
@@ -5,3 +5,15 @@ jenkins_password_credentials 'admin' do
   description 'Admin'
   password node[:jenkins][:password]
 end
+
+jenkins_plugin 'git'
+jenkins_plugin 'thinBackup'
+jenkins_plugin 'artifactdeployer'
+jenkins_plugin 'github'
+jenkins_plugin 'github-api'
+jenkins_plugin 'github-oauth'
+jenkins_plugin 'ghprb'
+jenkins_plugin 'mailer'
+jenkins_plugin 'parameterized-trigger'
+jenkins_plugin 's3'
+jenkins_plugin 'ws-cleanup'
